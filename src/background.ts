3191bf72bc2fe5d0d01f71556693697e1a1ab406
EnzoDFer <enzodfer@outlook.com>
2022-09-04 15:47:40 -0400

    Remove tutorial script ts and html

--- src/background.ts
+++
@@ -1,6 +0,0 @@
-let color:string = '#87CEEB';
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %c a color', `color: ${color}`);
-});
