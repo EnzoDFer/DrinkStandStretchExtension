7b2dc14e22a05ad38681ba91ba21f182d93173cf
EnzoDFer <enzodfer@outlook.com>
2022-09-04 15:31:53 -0400

    tTContinue practicing script functionality

--- src/menu.ts
+++ src/menu.ts
@@ -1,6 +1,29 @@
 let button:HTMLButtonElement|null = document.querySelector("#colorChange");
 
+if (button==null) throw new Error("No button found");
+
 chrome.storage.sync.get("color", ({ color }) => {
-  if (button==null) throw new Error("No button found");
-  button.style.backgroundColor = color;
+  if (button) button.style.backgroundColor = color;
 });
+
+// When the button is clicked, inject setPageBackgroundColor into current page
+button.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  
+  if (!tab.id) throw new Error("No tab found");
+
+  chrome.scripting.executeScript({
+    target: { 
+      tabId: tab.id 
+    },
+    func: setPageBackgroundColor,
+  });
+});
+
+// The body of this function will be executed as a content script inside the
+// current page
+function setPageBackgroundColor(): void {
+  chrome.storage.sync.get("color", ({ color }) => {
+    document.body.style.backgroundColor = color;
+  });
+}
