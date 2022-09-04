f19e6adefd31ec05154966df30a05e768e9c4cfc
EnzoDFer <enzodfer@outlook.com>
2022-09-04 17:27:08 -0400

    Create alarmClock class

--- src/menu.ts
+++ src/menu.ts
@@ -1,1 +1,30 @@
+interface IAlarm{};
+
+const alarmClocks: alarmClock[] = [];
+
+class alarmClock {
+  private _repeatDelayInMinutes: number;
+  public set repeatDelayInMinutes(delay:number) {
+    this._repeatDelayInMinutes = delay;
+  }
+  public get repeatDelayInMinutes() {
+    return this._repeatDelayInMinutes;
+  }
+
+  private _alarmName:string;
+  public get alarmName() {
+    return this._alarmName;
+  }
+
+  constructor(alarmName:string) {
+    this._repeatDelayInMinutes = 60;
+    this._alarmName = alarmName;
+    chrome.alarms.create(
+      alarmName,
+      {
+      periodInMinutes: this._repeatDelayInMinutes,
+    });
+    alarmClocks.push(this);
+  }
+}
 
